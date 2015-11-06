d637c84c1efe5b483460c0b0fbcd15cd20926dfb
Miguel Arango <djmiguelarango@gmail.com>
2015-11-06 03:19:50 +0000

    User, Ticket seeders with foreign keys

--- database/seeds/BaseSeeder.php
+++ database/seeds/BaseSeeder.php
@@ -1,12 +1,15 @@
 <?php
 
 use Faker\Generator;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Seeder;
 use Faker\Factory as Faker;
 
 
 abstract class BaseSeeder extends Seeder
 {
+    protected static $pool = [];
+
     protected function createMultiple($total, array $customValues = [])
     {
         for ($i = 1; $i <= $total; $i++) {
@@ -22,6 +25,30 @@ abstract class BaseSeeder extends Seeder
         $values = $this->getDummyData(Faker::create());
         $values = array_merge($values, $customValues);
 
-        $this->getModel()->create($values);
+        $this->addToPool($this->getModel()->create($values));
     }
+
+    protected function getRandom($model)
+    {
+        if (! isset(static::$pool[$model])) {
+            throw new Exception('The collection ' + $model + ' does not exist');
+        }
+
+        return static::$pool[$model]->random();
+    }
+
+    protected function addToPool($entity)
+    {
+        $reflection = new ReflectionClass($entity);
+        $class = $reflection->getShortName();
+
+        if (! isset(static::$pool[$class])) {
+            static::$pool[$class] = new Collection();
+        }
+
+        static::$pool[$class]->add($entity);
+
+        return $entity;
+    }
+
 }
