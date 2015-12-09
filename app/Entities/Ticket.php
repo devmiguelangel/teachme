fb31f2ed0ed94f9823d179d49ecd58447ff91c4d
Miguel Arango <djmiguelarango@gmail.com>
2015-12-09 22:45:32 +0000

    Relationships

--- app/Entities/Ticket.php
+++ app/Entities/Ticket.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
+    public function comments()
+    {
+        return $this->hasMany(TicketComment::class, 'ticket_id', 'id');
+    }
+
+    public function voters()
+    {
+        return $this->belongsToMany(User::class, 'ticket_votes', 'ticket_id', 'user_id');
+    }
+
     public function getOpenAttribute()
     {
         return $this->status == 'open';
--- app/Entities/User.php
+++ app/Entities/User.php
@@ -36,4 +36,9 @@ class User extends Model implements AuthenticatableContract,
      * @var array
      */
     protected $hidden = ['password', 'remember_token'];
+
+    public function tickets()
+    {
+        return $this->belongsToMany(Ticket::class, 'ticket_votes', 'user_id', 'ticket_id');
+    }
 }
